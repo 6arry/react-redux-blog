3a6ce9299304fee6e568dc8d2097ca63b2ed8325
gccunanan <gccunanan@gmail.com>
2019-12-31 16:54:39 -0800

    completed basic blog app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    // console.log('About to fetch posts...')
+    await dispatch(fetchPosts())
+    // console.log('fetch posts!')
+    // console.log(getState().posts)
+   const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    // console.log(userIds)
+    userIds.forEach(id => dispatch(fetchUser(id)))
+}
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
@@ -6,8 +17,16 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({ type: 'FETCH_USER', payload: response.data })
 }
+
+// export const fetchUser = (id) =>  dispatch => _fetchUser(id, dispatch)
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`)
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// })
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchUser } from '../actions'
 
 class UserHeader extends Component {
-    componentDidMount(){
-        this.props.fetchUser(this.props.userId)
-    }
 
     render() {
         const { user } = this.props
-        
+
         if (!user){
             return null
         }
@@ -26,5 +22,5 @@ const mapStateToProps = (state, ownProps) => {
     return { user: state.users.find(user => user.id === ownProps.userId) }
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader)
+export default connect(mapStateToProps)(UserHeader)
 
